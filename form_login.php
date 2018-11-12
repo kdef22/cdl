7783305a3fe68d35841053871893bd0180945449
tafarel <mayron75@gmail.com>
2018-11-12 21:58:27 -0200

    Initial commit

---
+++ form_login.php
@@ -0,0 +1,16 @@
+<fieldset class="mt-5 col-12 mx-auto col-sm-4 card p-3">
+  <legend class="text-center">Login</legend>
+  <form action="" method="post">
+    <div class="form-group">
+      <label for="iEmail" class="sr-only">Email</label>
+      <input type="email" id="iEmail" name="txtEmail" placeholder="Informe o email" class="form-control" required>
+    </div>
+    <div class="form-group">
+      <label for="iPass" class="sr-only">Senha</label>
+      <input type="password" id="iEiPassmail" name="txtPass" placeholder="Informe a senha" class="form-control" required>
+    </div>
+    <div>
+      <button class="btn btn-dark btn-block col-12 col-sm-3">Sign in</button>
+    </div>
+  </form>
+</fieldset>
